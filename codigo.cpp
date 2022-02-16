c146a5993a2d9393e00eb9c0e700a1cafe7fd0dd
Hashem <43625073+lesqueda@users.noreply.github.com>
2022-02-15 21:32:13 -0500

    initial commit

---
+++ codigo.cpp
@@ -0,0 +1,35 @@
+}while(numero<=10);
+
+
+
+if(condicion){
+
+    if(condicion){
+
+
+
+    }
+
+}
+
+
+
+int a = 10;;
+
+while(condicion){
+
+
+
+
+
+    for(int i=0; i<=10; i++){
+
+        for(int l = 0; l <=10; l++){
+
+
+
+        }
+
+    }
+
+}
